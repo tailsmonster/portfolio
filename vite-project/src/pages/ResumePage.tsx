029315988984f166ed4b5d792a4e0ebefdade765
tailsmonster12 <aroca.twelve@proton.me>
2025-02-06 19:50:20 -0500

    added resume pdf to assets

--- vite-project/src/pages/ResumePage.tsx
+++ vite-project/src/pages/ResumePage.tsx
@@ -1,3 +1,14 @@
-const ResumePage = () => <h1>Heres my resume!</h1>;
+import React from "react";
+
+interface Props {};
+
+const ResumePage = (props: Props) => {
+  return (
+    <>
+      <h1>Welcome!</h1>;
+      <embed src="https://drive.google.com/viewerng/viewer?embedded=true&url=http://example.com/the.pdf" width="500" height="375"></embed>
+    </>
+  )
+  }
 
 export default ResumePage
